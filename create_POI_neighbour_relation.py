1c65328354b457d98eae0a39d39bf38da321934e
Thierry Morel <50456999+titicalvados@users.noreply.github.com>
2023-08-23 18:34:15 +0200

    Fix error in file

--- create_POI_neighbour_relation.py
+++ create_POI_neighbour_relation.py
@@ -18,18 +18,14 @@ collection_poi = datatourisme.get_collection(name="poi")
 nb_poi = collection_poi.count_documents({})
 print("Nombre de POIs: ",nb_poi)
 
-
 allowedTypes = ["Accommodation","Visit","Rental","Store","FoodEstablishment","EntertainmentAndEvent","SportsAndLeisurePlace","CulturalSite","Tour","CampingAndCaravanning","ReligiousSite","NaturalHeritage","TouristInformationCenter"] 
 
 userMostSelectedTypes = ["Accommodation","FoodEstablishment","EntertainmentAndEvent","SportsAndLeisurePlace"]
 userLeastSelectedTypes = ["Rental","Store","NaturalHeritage","TouristInformationCenter"]
-userSelectedTypes = ["EntertainmentAndEvent","SportsAndLeisurePlace","CulturalSite","Tour","ReligiousSite","Store","NaturalHeritage"]
+userSelectedTypes = ["EntertainmentAndEvent"]
 
 
-pois = collection_poi.aggregate([{'$match': { 'types' : {'$in':['Accommodation','FoodEstablishment','EntertainmentAndEvent','SportsAndLeisurePlace']}}},{'$project': { 'identifier': '$dc:identifier','longitude':'$longitude','latitude':'$latitude'}}])
-
-#for poi in pois:
-#    print(poi)
+pois = collection_poi.aggregate([{'$match': { 'types' : {'$in':userSelectedTypes}}},{'$project': { 'identifier': '$dc:identifier','longitude':'$longitude','latitude':'$latitude'}}])
 
 orArray=[]
 for type in userSelectedTypes:
@@ -49,12 +45,13 @@ with driver_neo4j.session() as session:
     result = session.run(query).data()
 
 #retrieve neighbours for all nodes and create relation with distance weight
+neighbourhoodDistance = 50000
+print("considéré comme voisin (en m) si moins de :",neighbourhoodDistance)
 for row in pois:
     #filtrage sur les erreurs latitude longitude hors France métropolaine
     if not (51.10>= float(row["latitude"])>=41.30) or not (9.57>= float(row["longitude"]) >=-5.17):
         continue
-    #orArray = [{ 'types': 'Accommodation' },{'types': 'FoodEstablishment'}]
-    neighbours = collection_poi.aggregate([{'$geoNear': {'near': { 'coordinates': [ -4.75698 , 48.520412 ] },'distanceField': 'distance','maxDistance': 40000,'query': { '$or': orArray}}},{'$project': { 'identifier': '$dc:identifier','distance':'$distance'}}])
+    neighbours = collection_poi.aggregate([{'$geoNear': {'near': { 'coordinates': [float(row["longitude"]) , float(row["latitude"])] },'distanceField': 'distance','maxDistance': neighbourhoodDistance,'query': { '$or': orArray}}},{'$project': { 'identifier': '$dc:identifier','distance':'$distance'}}])
     for neighbour in neighbours:
         if neighbour["identifier"] == row["identifier"]:
             continue
@@ -63,7 +60,7 @@ for row in pois:
             query = '''
             MATCH (p:POI) where p.id = $row_id
             MATCH (q:POI) where q.id = $neighbour_identifier
-            MERGE (q)-[r:NEIGHBOUR]-(p)
+            MERGE (p)-[r:NEIGHBOUR]->(q)
             SET r.distance = $distance
             '''
             result = session.run(query, row_id=row["identifier"],neighbour_identifier=neighbour["identifier"],distance = neighbour["distance"]).data()
