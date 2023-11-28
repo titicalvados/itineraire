4dbb34479aa3fcc14f1c802608e7749cf4e9c6a3
Thierry Morel <50456999+titicalvados@users.noreply.github.com>
2023-11-28 15:52:58 +0100

    Update POIs_infos_exposure.py
    Comments removed

--- POIs_infos_exposure.py
+++ POIs_infos_exposure.py
@@ -24,7 +24,7 @@ print("Nombre de POIs: ",nb_poi)
 
 @app.route("/getpoiinfos/<id>")
 def get_poi_infos(id):
-    poi = collection_poi.aggregate([{'$match'  : {'dc:identifier':id}},{'$project': { '_id':0,'identifier': '$dc:identifier','label': '$label','types': '$types','shortDescription':'$shortDescription','locality': '$addressLocality','postalCode':'$postalCode','email':'$email','telephone1':'$telephone1','web':'$web','latitude':'$latitude','longitude':'$longitude'}}])
+    poi = collection_poi.aggregate([{'$match'  : {'dc:identifier':id}},{'$project': { '_id':0,'identifier': '$dc:identifier','label': '$label','types': '$types','comment':'$comment','shortDescription':'$shortDescription','locality': '$addressLocality','postalCode':'$postalCode','email':'$email','telephone1':'$telephone1','web':'$web','latitude':'$latitude','longitude':'$longitude'}}])
     return Response(dumps(list(poi)[0]), mimetype='application/json')
 
 
@@ -34,21 +34,10 @@ def get_pois_list(type):
     return Response(dumps(list(poi)),mimetype='application/json')
 
 
-#poislist = collection_poi.aggregate([{'$match'  : {'types':{'$in' : [types_list]}}},{'$project': { 'identifier': '$dc:identifier'}}])
-
-poi = collection_poi.aggregate([{'$match'  : {'dc:identifier':"FMABRE029V53N109"}},{'$project': { 'identifier': '$dc:identifier','label': '$label','types': '$types','locality': '$addressLocality','postalCode':'$postalCode','latitude':'$latitude','longitude':'$longitude'}}])
-
-#pois = collection_poi.aggregate([{'$project': { 'identifier': '$dc:identifier','label': '$label','types': '$types','locality': '$addressLocality','postalCode':'$postalCode','latitude':'$latitude','longitude':'$longitude'}}])
-
-
-print(list(poi))
-
-
 selectedTypes = ["AccommodationProduct","Visit","Rental","Store","Accommodation","FoodEstablishment","EntertainmentAndEvent","SportsAndLeisurePlace","CulturalSite","Tour","CampingAndCaravanning","ReligiousSite","NaturalHeritage","TouristInformationCenter"]
 
 tt = time() - t0
 print("Réalisé en {} secondes".format(round(tt,3)))
-
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0",debug=True)
