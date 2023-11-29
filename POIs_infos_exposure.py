2f7e0503bcd42a6d24e8c74a81a3ff73150dd447
Thierry Morel <50456999+titicalvados@users.noreply.github.com>
2023-11-29 10:45:33 +0100

    Update POIs_infos_exposure.py with 2 endpoints
    Updated according to specs with the 4 endpoints

--- POIs_infos_exposure.py
+++ POIs_infos_exposure.py
@@ -1,11 +1,13 @@
-import json, os, requests, jsonify, json
+import json, os, requests, json
 from werkzeug.wrappers import Response
 from bson import json_util
 from fnmatch import fnmatch
 from pymongo import MongoClient
 from time import time
-from flask import Flask
+from flask import Flask, jsonify
 from bson.json_util import dumps
+from pydantic import BaseModel
+from flask_pydantic import validate 
 
 client = MongoClient(host="127.0.0.1", port = 27017)
 
@@ -13,7 +15,11 @@ datatourisme = client["datatourisme"]
 
 collection_poi = datatourisme.get_collection(name="poi")
 
-t0 = time()
+selectedTypes = ["AccommodationProduct","Visit","Rental","Store","Accommodation","FoodEstablishment","EntertainmentAndEvent","SportsAndLeisurePlace","CulturalSite","Tour","CampingAndCaravanning","ReligiousSite","NaturalHeritage","TouristInformationCenter"]
+
+class Query(BaseModel):
+    id:str
+    neighbourhoodDistance:int 
 
 app = Flask(__name__)
 
@@ -21,23 +27,29 @@ app = Flask(__name__)
 nb_poi = collection_poi.count_documents({})
 print("Nombre de POIs: ",nb_poi)
 
-
 @app.route("/getpoiinfos/<id>")
 def get_poi_infos(id):
     poi = collection_poi.aggregate([{'$match'  : {'dc:identifier':id}},{'$project': { '_id':0,'identifier': '$dc:identifier','label': '$label','types': '$types','comment':'$comment','shortDescription':'$shortDescription','locality': '$addressLocality','postalCode':'$postalCode','email':'$email','telephone1':'$telephone1','web':'$web','latitude':'$latitude','longitude':'$longitude'}}])
     return Response(dumps(list(poi)[0]), mimetype='application/json')
 
+@app.route("/getpoineighbours")
+@validate()
+def get_poi_neighbours(query:Query):
+    poi = collection_poi.aggregate([{'$match'  : {'dc:identifier':query.id}},{'$project': { '_id':0,'identifier': '$dc:identifier','latitude':'$latitude','longitude':'$longitude'}}])
+    coord=list(poi)[0]
+    long=coord["longitude"]
+    lat=coord["latitude"]
+    neighbours = collection_poi.aggregate([{'$geoNear': {'near': { 'coordinates': [float(long) , float(lat)] },'distanceField':'distance','maxDistance': query.neighbourhoodDistance }},{'$project': { '_id':0 ,'origin': query.id, 'end': '$dc:identifier', 'distance':'$distance'}}])
+    return Response(dumps(list(neighbours)),mimetype='application/json')
+
+@app.route("/gettypeslist")
+def get_types_list():
+    return jsonify(types = selectedTypes)
 
 @app.route("/getpoislistbytype/<type>")
 def get_pois_list(type):
     poi = collection_poi.aggregate([{'$match'  : {'types':{'$in' : [type]}}},{'$project': { '_id':0,'identifier': '$dc:identifier','latitude':'$latitude','longitude':'$longitude'}}])
     return Response(dumps(list(poi)),mimetype='application/json')
-
-
-selectedTypes = ["AccommodationProduct","Visit","Rental","Store","Accommodation","FoodEstablishment","EntertainmentAndEvent","SportsAndLeisurePlace","CulturalSite","Tour","CampingAndCaravanning","ReligiousSite","NaturalHeritage","TouristInformationCenter"]
-
-tt = time() - t0
-print("Réalisé en {} secondes".format(round(tt,3)))
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0",debug=True)
