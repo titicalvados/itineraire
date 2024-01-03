d024347d05bbc48226c33eb84d1001590e2f55fd
Thierry Morel <50456999+titicalvados@users.noreply.github.com>
2024-01-03 11:52:37 +0100

    Update 2_extract_datatourisme.py

--- docker_extract_datatourisme/2_extract_datatourisme.py
+++ docker_extract_datatourisme/2_extract_datatourisme.py
@@ -1,6 +1,7 @@
 import os
 import requests
 import zipfile
+from time import time
 from datetime import datetime
 from flask import Flask, jsonify
 import shutil
@@ -12,16 +13,17 @@ def create_and_clear_directory(directory):
     # Supprimer le répertoire et tout son contenu s'il existe
     if os.path.exists(directory):
         shutil.rmtree(directory)
-    # Le créer
+    # le créer
     os.makedirs(directory)
 
 @app.route('/telecharger-et-archiver', methods=['GET'])
 def download_and_archive():
+    t0 = time()
     url = "https://diffuseur.datatourisme.fr/webservice/faa865a44fc4b68d82b54901dcb692ee/f39995e9-0c1a-4aee-bc57-7338d1ef5eef"
-    local_dir = os.environ.get('LOCAL_DIR', 'path_non_defini')
     # Créer un répertoire avec la date du jour
     date_today = datetime.now().strftime('%Y-%m-%d')
-    destination_dir = os.path.join(local_dir, date_today)
+    destination_dir = os.path.join('/home/datatourisme', date_today)
+    print("répertoire d'importation:",destination_dir) 
     
     # Créer et vider le répertoire
     create_and_clear_directory(destination_dir)
@@ -30,16 +32,21 @@ def download_and_archive():
 
     # Télécharger le fichier .zip
     response = requests.get(url)
-    with open(local_file_path, 'wb') as file:
-        file.write(response.content)
+    
+    if response.status_code == 200:
+        with open(local_file_path, 'wb') as file:
+            file.write(response.content)
 
-    # Extraire le contenu du .zip
-    with zipfile.ZipFile(local_file_path, 'r') as zip_ref:
-        zip_ref.extractall(destination_dir)
+        # Extraire le contenu du .zip
+        with zipfile.ZipFile(local_file_path, 'r') as zip_ref:
+            zip_ref.extractall(destination_dir)
 
-    # Supprimer le fichier .zip
-    os.remove(local_file_path)
+        # Supprimer le fichier .zip
+        os.remove(local_file_path)
+    
+    tt = time() - t0
+    status = 'Import datatourisme fait en {} secondes'.format(round(tt,3))
+    return jsonify({'datatourisme_response_code':response.status_code,'status':status,'local_directory': destination_dir})
 
-    return jsonify({'local_directory': destination_dir})
 if __name__ == '__main__':
     app.run(debug=True, port=5002)
