0f9526382c1368b029f7329eb9a2a1694bb47e2c
Thierry Morel <50456999+titicalvados@users.noreply.github.com>
2024-01-03 11:57:46 +0100

    Update 1_service_manager.py

--- docker_service_manager/1_service_manager.py
+++ docker_service_manager/1_service_manager.py
@@ -55,13 +55,13 @@ app.layout = html.Div(
             style={'margin':'auto','text-align':'center'}
         ),
         html.Div(
-            dcc.Dropdown(
+            dcc.Dropdown(id='Dropdown',
                 options= [
+                    {'label': 'Sélectionner le type de POI pour créer les relations Neo4J', 'value': 'no_action'},
                     {'label': 'Créer relations Neo4J pour EntertainmentAndEvent', 'value': 'val_event'},
                     {'label': 'Créer relations Neo4J pour CulturalSite', 'value': 'val_cult'},
-                    {'label': 'Créer relations Neo4J pour Accomodation', 'value': 'val_acco'}],
-                id = 'iti_theme',
-                value= 'val_event'
+                    {'label': 'Créer relations Neo4J pour Accommodation', 'value': 'val_acco'}],
+                value= 'no_action'
             ),
             style={'margin':'2% auto auto auto','textAlign': 'center', 'color': 'mediumturquoise','opacity': '0.0 - 1.0', 'width':'40%'}
         ),
@@ -77,11 +77,12 @@ app.layout = html.Div(
     ]
 )
 
-@app.callback(Output('request_result_info', 'children'),
+@app.callback(Output('request_result_info', 'children', allow_duplicate=True),
               Input('import_web', 'n_clicks'),
               Input('etl_mongo', 'n_clicks'),
               Input('detect_clusters', 'n_clicks'),
-              Input('import_neo4j', 'n_clicks')
+              Input('import_neo4j', 'n_clicks'),
+              prevent_initial_call=True
 )
 def displayClick(btn1, btn2, btn3, btn4):
     msg = "Waiting for request result"
@@ -120,9 +121,60 @@ def displayClick(btn1, btn2, btn3, btn4):
             else:
                 msg = "microservice 4 response code "+str(response.status_code)
     elif "import_neo4j" == ctx.triggered_id:
-        msg = "neo4j was most recently clicked"
+        api_url = "http://localhost:5006/loadinneo4j"
+        try:
+            response = requests.get(api_url)
+        except:
+            msg = "microservice 6 non accessible"
+        else:
+            if response.status_code == 200:
+                msg = json.dumps(response.json())
+            else:
+                msg = "microservice 6 response code "+str(response.status_code)
     return msg
 
+@app.callback(Output('request_result_info', 'children'),
+              Input('Dropdown', 'value')
+)
+def createRelations(type):
+    msg = "Waiting for request result"
+    if type == "no_action":
+        msg = "no action"
+    if type == "val_event":
+        # URI microservice
+        api_url = "http://localhost:5006/createrelations?type=EntertainmentAndEvent&maxdistance=30&mindistance=25"
+        try:
+            response = requests.get(api_url)
+        except:
+            msg = "microservice 6 non accessible"
+        else:
+            if response.status_code == 200:
+                msg = json.dumps(response.json())
+            else:
+                msg = "microservice 6 response code "+str(response.status_code)
+    elif type == "val_cult":
+        api_url = "http://localhost:5006/createrelations?type=CulturalSite&maxdistance=30&mindistance=25"
+        try:
+            response = requests.get(api_url)
+        except:
+            msg = "microservice 6 non accessible"
+        else:
+            if response.status_code == 200:
+                msg = json.dumps(response.json())
+            else:
+                msg = "microservice 6 response code "+str(response.status_code)
+    elif type == "val_acco":
+        api_url = "http://localhost:5006/createrelations?type=Accommodation&maxdistance=30&mindistance=25"
+        try:
+            response = requests.get(api_url)
+        except:
+            msg = "microservice 6 non accessible"
+        else:
+            if response.status_code == 200:
+                msg = json.dumps(response.json())
+            else:
+                msg = "microservice 6 response code "+str(response.status_code)
+    return msg
 
 if __name__ == '__main__':
   app.run_server(debug=True, host='0.0.0.0', port=5001)
